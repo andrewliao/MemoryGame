5c90f7daf31dd2fa61f489e838493a7972b6773f
Andrew <andrew.sf.liao@gmail.com>
2018-02-17 14:17:00 -0500

    adding kevins changes

--- year1/MemoryGame.java
+++ year1/MemoryGame.java
@@ -14,29 +14,137 @@ import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.scene.image.Image ;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Rectangle;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import javax.swing.JOptionPane;
+import javafx.scene.layout.BorderPane;
+import javafx.animation.Animation;
+import java.lang.reflect.InvocationTargetException;
 
 public class MemoryGame extends Application {
-	/** this stores the default number of rows in the board */
-	public static int numRows = 12;
-	/** this stores the default number of columns in the board */
-	public static int numColumns = 12;
-
-	Button button;
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
-	
-	@Override
-	public void start(Stage primaryStage) {
-		primaryStage.setTitle("Title of the window");
-		
-		
-		GridPane layout = new GridPane();
-		
-		Scene scene = new Scene(layout);
-		primaryStage.setScene(scene);
-		primaryStage.show();
-		
-	}
+    /** this stores the default number of rows in the board */
+    public static int numRows = 12;
+    /** this stores the default number of columns in the board */
+    public static int numColumns = 12;
+    
+    private final Color[] predefinedColors = {
+    Color.rgb(64, 163, 63),
+    Color.rgb(245, 152, 29),
+    Color.rgb(252, 88, 52),
+    Color.rgb(27, 114, 222),
+    Color.rgb(203, 0, 78),
+    Color.rgb(137, 0, 160)
+    };
+    
+    /** this stores buttons to be added to the game */
+    private Button[][] buttons = null;
+    
+    Button button;
+    
+    public Color[] getPredefinedColors(){
+        return predefinedColors;
+    }
+    
+    public void setButtons(Button[][] buttons){
+        this.buttons = buttons;
+    }
+    
+    public Button[][] getButtons(){
+        return this.buttons;
+    }
+    
+    public int[] locate(Button b){
+        /** this stores all buttons in the play area */
+        Button[][] checkButtons = this.getButtons();
+        
+        /** this stores the row index of clicked button */
+        Integer rowCheck = null;
+        /** this b stores the column index of clicked button */
+        Integer columnCheck = null;
+        /**
+         * this loop iterates all buttons and finds the one matches the one user clicks
+         * subgoal: tries to find the matched button in one column
+         */
+        for (int column = 0; column < checkButtons.length; column ++){
+            for (int row = 0; row < checkButtons[0].length; row ++ ){
+                if (checkButtons[column][row] == b) {
+                    columnCheck = column;
+                    rowCheck = row;
+                }
+            }
+        }
+        /** this stores coordinate info of the clicked button */
+        int[] coordinate = { columnCheck, rowCheck };
+        return coordinate;
+    }
+    
+    public GridPane setupPlayArea(int intendedColumn, int intendedRow, int numColors){
+        /** this stores the grid pane where all buttons are placed on */
+        GridPane gridPane = new GridPane();
+        try{
+            this.setButtons(new Button[intendedColumn][intendedRow]);
+            
+            /** this stores the list of colors with the number the user intends to have */
+            Color[] colors = new Color[numColors];
+            
+            /**
+             * this loop extracts colors from the predefined colors with the correct amount
+             * subgoal: retrieve the predefined colors
+             */
+            for (int i = 0; i < numColors; i ++){
+                colors[i] = this.getPredefinedColors()[i];
+            }
+            
+            /**
+             * this loop setups the play area with the correct amount of buttons, colors and their actions
+             * subgoal: initialize buttons, set fills, make background transparent, add to the gridpane, and set actions
+             */
+            for (int column = 0; column < intendedColumn; column ++){
+                for (int row = 0; row < intendedRow; row++){
+                    this.getButtons()[column][row] = new Button();
+                    this.getButtons()[column][row].setPrefSize(50, 50);
+                    
+                    /** this stores the rectangle to be placed into buttons */
+                    Rectangle rectangle = new Rectangle(50, 50);
+                    rectangle.setArcHeight(15);
+                    rectangle.setArcWidth(rectangle.getArcHeight());
+                    rectangle.setFill(colors[(int)(Math.random() * numColors)]);
+                    
+                    this.getButtons()[column][row].setGraphic(rectangle);
+                    this.getButtons()[column][row].setStyle("-fx-background-color: transparent");
+                    
+                    gridPane.add(this.getButtons()[column][row], column, row);
+                    
+                    this.getButtons()[column][row].setOnAction(e ->{
+                    });
+                }
+            }
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Too many colors! Make them less than 7");
+        }
+        
+        return gridPane;
+    }
+    
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Scene scene = new Scene(this.setupPlayArea(12, 12, 3));
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
