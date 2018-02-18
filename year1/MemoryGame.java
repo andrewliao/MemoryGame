32c1db3368f184bc91edf1798b855fecf502730b
Kefan <likefan052@gmail.com>
2018-02-17 19:18:56 -0500

    halfway finish the compare method

--- year1/MemoryGame.java
+++ year1/MemoryGame.java
@@ -31,7 +31,7 @@ import javafx.scene.layout.BorderPane;
 import javafx.animation.Animation;
 import java.lang.reflect.InvocationTargetException;
 
-public class MemoryGame extends Application {
+public class Main extends Application {
     /** this stores the default number of rows in the board */
     private static int numRows = 4;
     /** this stores the default number of columns in the board */
@@ -42,52 +42,73 @@ public class MemoryGame extends Application {
     private int[][] colorIndex = new int[8][1];
     /** this stores the hidden colors of the buttons */
     private Color[][] buttonColor = new Color[4][4];
+
+    private int[] coordinateTocheck;
+
+    private int numberOfButtonFlipped = 0;
+
     /** This is the default color when it is not flipped yet */
     private final Color lightGrayColor = Color.rgb(225, 225, 225);
-    
+
+
     /** This stores the predifinedColors we can use */
     private final Color[] predefinedColors = {
-    Color.rgb(64, 163, 63),
-    Color.rgb(245, 152, 29),
-    Color.rgb(252, 88, 52),
-    Color.rgb(27, 114, 222),
-    Color.rgb(203, 0, 78),
-    Color.rgb(137, 0, 160),
-    Color.rgb(114, 85, 73),
-    Color.rgb(66, 66, 66),
-    Color.rgb(58, 71, 78)
+            Color.rgb(64, 163, 63),
+            Color.rgb(245, 152, 29),
+            Color.rgb(252, 88, 52),
+            Color.rgb(27, 114, 222),
+            Color.rgb(203, 0, 78),
+            Color.rgb(137, 0, 160),
+            Color.rgb(114, 85, 73),
+            Color.rgb(66, 66, 66),
+            Color.rgb(58, 71, 78)
     };
-    
+
     /** this stores buttons to be added to the game */
     private Button[][] buttons;
-    
+
     /** this sets up the default boolean[][] indicating whether or not something is flipped */
     public void defaultBoolean() {
-    		for(int i = 0; i < flipped.length; i++) {
-    			for(int j = 0; j < flipped[0].length; j++) {
-    				flipped[i][j] = false;
-    			}
-    		}
+        for(int i = 0; i < flipped.length; i++) {
+            for(int j = 0; j < flipped[0].length; j++) {
+                flipped[i][j] = false;
+            }
+        }
     }
-    
+
+    public void setNumberOfButtonFlipped(int number){
+        this.numberOfButtonFlipped = number;
+    }
+
+    public int getNumberOfButtonFlipped(){
+        return this.numberOfButtonFlipped;
+    }
+
     /** this method returns the array of the predefinedColors */
     public Color[] getPredefinedColors(){
         return predefinedColors;
     }
-    
-    
+
+    public void setCoordinateToCheck(int[] coordinate){
+        this.coordinateTocheck = coordinate;
+    }
+
+    public int[] getCoordinateToCheck(){
+        return this.coordinateTocheck;
+    }
+
     public void setButtons(Button[][] buttons){
         this.buttons = buttons;
     }
-    
+
     public Button[][] getButtons(){
         return this.buttons;
     }
-    
+
     public int[] locate(Button b){
         /** this stores all buttons in the play area */
         Button[][] checkButtons = this.getButtons();
-        
+
         /** this stores the row index of clicked button */
         Integer rowCheck = null;
         /** this b stores the column index of clicked button */
@@ -108,16 +129,20 @@ public class MemoryGame extends Application {
         int[] coordinate = { columnCheck, rowCheck };
         return coordinate;
     }
-    
+
+    public boolean compare(int[] coordinate){
+        return buttonColor[coordinate[0]][coordinate[1]] == ((Rectangle)(this.getButtons()[this.getCoordinateToCheck()[0]][this.getCoordinateToCheck()[1]].getGraphic())).getFill();
+    }
+
     public GridPane setupPlayArea(int intendedColumn, int intendedRow, int numColors){
         /** this stores the grid pane where all buttons are placed on */
         GridPane gridPane = new GridPane();
         try{
             this.setButtons(new Button[intendedColumn][intendedRow]);
-            
+
             /** this stores the list of colors with the number the user intends to have */
             Color[] colors = new Color[numColors];
-            
+
             /**
              * this loop extracts colors from the predefined colors with the correct amount
              * subgoal: retrieve the predefined colors
@@ -125,72 +150,84 @@ public class MemoryGame extends Application {
             for (int i = 0; i < numColors; i++){
                 colors[i] = this.getPredefinedColors()[i];
             }
-            
+
             /**
              * this loop setups the play area with the correct amount of buttons, colors and their actions
              * subgoal: initialize buttons, set fills, make background transparent, add to the gridpane, and set actions
              */
             for (int column = 0; column < intendedColumn; column ++){
                 for (int row = 0; row < intendedRow; row++){
-                		
-                		//filling all the buttons to be the default color of 
+
+                    //filling all the buttons to be the default color of
                     this.getButtons()[column][row] = new Button();
-                    this.getButtons()[column][row].setPrefSize(50, 50);
-                    
+                    this.getButtons()[column][row].setPrefSize(100, 100);
+
                     /** this stores the rectangle to be placed into buttons */
-                    Rectangle rectangle = new Rectangle(50, 50);
+                    Rectangle rectangle = new Rectangle(100, 100);
                     rectangle.setArcHeight(15);
                     rectangle.setArcWidth(rectangle.getArcHeight());
                     boolean colorFound = false;
                     while(!colorFound) {
-                   		int indexOfColor = (int)(Math.random() * colorIndex.length);
-                   		if(colorIndex[indexOfColor][0] < 2) {
-                   			rectangle.setFill(lightGrayColor);
-                    			colorIndex[indexOfColor][0]++;
-                    			buttonColor[row][column] = colors[indexOfColor];
-                    				colorFound = true;
-                    	}
-                    			
-                  }
-                    
-                   
+                        int indexOfColor = (int)(Math.random() * colorIndex.length);
+                        if(colorIndex[indexOfColor][0] < 2) {
+                            rectangle.setFill(lightGrayColor);
+                            colorIndex[indexOfColor][0]++;
+                            buttonColor[row][column] = colors[indexOfColor];
+                            colorFound = true;
+                        }
+
+                    }
+
+
                     this.getButtons()[column][row].setGraphic(rectangle);
                     this.getButtons()[column][row].setStyle("-fx-background-color: transparent");
-                    
+
                     gridPane.add(this.getButtons()[column][row], column, row);
-                    
+
                     this.getButtons()[column][row].setOnAction(e ->{
-                    	/** this stores the clicked button instance */
+                        this.setNumberOfButtonFlipped(this.getNumberOfButtonFlipped()+1);
+
+                        /** this stores the clicked button instance */
                         Button b = (Button)e.getSource();
                         /** this stores the coordinate of the clicked button */
-                        int[] coordinate = this.locate(b);   
-                        ((Rectangle)b.getGraphic()).setFill(buttonColor[coordinate[0]][coordinate[1]]);
+                        int[] coordinate = this.locate(b);
+                        if (this.getNumberOfButtonFlipped() % 2 != 0){
+                            ((Rectangle)b.getGraphic()).setFill(buttonColor[coordinate[0]][coordinate[1]]);
+                            this.setCoordinateToCheck(coordinate);
+                        }else{
+                            if (this.compare(coordinate)){
+                                ((Rectangle)b.getGraphic()).setFill(buttonColor[coordinate[0]][coordinate[1]]);
+                                this.setNumberOfButtonFlipped(this.getNumberOfButtonFlipped() + 1);
+                            }else{
+                                this.setNumberOfButtonFlipped(this.getNumberOfButtonFlipped() - 1);
+                            }
+                        }
                     });
                 }
             }
         }catch(ArrayIndexOutOfBoundsException e){
             System.out.println("Too many colors! Make them less than 7");
         }
-        
+
         return gridPane;
     }
-    
-    
+
+
     @Override
     public void start(Stage primaryStage) {
-    		defaultBoolean();
-    		primaryStage.setTitle("Memory Game");
+        defaultBoolean();
+        primaryStage.setTitle("Memory Game");
         Scene scene = new Scene(this.setupPlayArea(4, 4, 8));
         primaryStage.setScene(scene);
         primaryStage.show();
         for(int i = 0 ; i < flipped.length; i++) {
-        		for(int j = 0; j < flipped[0].length; j++) {
-        			System.out.print(buttonColor[i][j] + " ");
-        		}
-        		System.out.println();
+            for(int j = 0; j < flipped[0].length; j++) {
+                System.out.print(buttonColor[i][j] + " ");
+            }
+            System.out.println();
         }
     }
-    
+
     public static void main(String[] args) {
         launch(args);
     }
